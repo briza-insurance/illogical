c44ebc97889cc9d7cb398b30706051f30ee86b21
Menghao Yu <myu@briza.com>
2023-12-12 15:00:57 -0500

    GUS-864 Update Dependencies including ESM

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import { eslint } from 'rollup-plugin-eslint'
+import eslint from '@rollup/plugin-eslint'
 
 const libraryName = 'illogical'
 const input = './src/index.ts'
@@ -18,18 +18,18 @@ const plugins = [
 ]
 
 export default
-{
-  input,
-  output: [
-    {
-      file: './lib/' + libraryName + '.esm.js',
-      format: 'esm'
-    },
-    {
-      file: './lib/' + libraryName + '.js',
-      format: 'cjs',
-      exports: 'named'
-    }
-  ],
-  plugins
-}
+  {
+    input,
+    output: [
+      {
+        file: './lib/' + libraryName + '.esm.js',
+        format: 'esm'
+      },
+      {
+        file: './lib/' + libraryName + '.js',
+        format: 'cjs',
+        exports: 'named'
+      }
+    ],
+    plugins
+  }
