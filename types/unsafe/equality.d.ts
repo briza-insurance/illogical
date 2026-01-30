38e60db548ca7fe9368193345ca77318c05d601e
Andre Toledo M. Souza <andre.galla@gmail.com>
2026-01-30 11:45:07 -0300

    Missed deleting types

--- types/unsafe/equality.d.ts
+++
@@ -1,5 +0,0 @@
-import { Evaluable } from '../common/evaluable';
-import { Input } from '../parser';
-import { Options } from '../parser/options';
-export declare const simplifyEq: (opts: Options, simplifyInput: (input: Input) => Input | Evaluable) => (input: Input[] | [string, ...Input[]]) => Input | Evaluable;
-export declare const simplifyNe: (opts: Options, simplifyInput: (input: Input) => Input | Evaluable) => (input: Input[] | [string, ...Input[]]) => Input | Evaluable;
--- types/unsafe/xor.d.ts
+++
@@ -1,4 +0,0 @@
-import { Evaluable } from '../common/evaluable';
-import { Input } from '../parser';
-import { Options } from '../parser/options';
-export declare const simplifyXor: (opts: Options, simplifyInput: (input: Input) => Input | Evaluable) => (input: Input[] | [string, ...Input[]]) => Input | Evaluable;
